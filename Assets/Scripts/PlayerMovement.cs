d3864f066928ab17036c071f1ed63d9cccd9f29d
Paulo Celis <pauloxyo@gmail.com>
2020-12-04 11:32:18 -0600

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+    public float movementSpeed =1;
+    public float gravity = 9.8f;
+    private float velocity = 0;
+    private Camera cam;
+ 
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+ 
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        float vertical = Input.GetAxis("Vertical") * movementSpeed;
+        characterController.Move((cam.transform.right * horizontal + cam.transform.forward * vertical) * Time.deltaTime);
+ 
+        if(characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity -= gravity * Time.deltaTime;
+            characterController.Move(new Vector3(0, velocity, 0));
+        }
+    }
+}
