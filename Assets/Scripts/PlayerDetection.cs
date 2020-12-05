5e29f1d979092ce6cf1a60d047b4eb20ec3c84ee
Paulo Celis <pauloxyo@gmail.com>
2020-12-05 15:58:11 -0600

    Teleports

---
+++ Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public GameObject teleport;
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Teleport"){
+            string name = col.gameObject.GetComponent<Teleport>().getTeleportName();
+            teleport.GetComponent<SceneController>().LoadLevel(name);
+        }
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public string teleportName;
+
+    public string getTeleportName(){
+        return teleportName;
+    }
+}
