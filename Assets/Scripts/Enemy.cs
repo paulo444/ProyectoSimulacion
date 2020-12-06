2125fd471856b2a5527ba129c90c5e70662071c3
iZ-HK <mayzo2014i@gmail.com>
2020-12-06 17:09:39 -0600

    Agregado del modelado y el text de la vida en enemigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,22 +6,26 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent nav;
+    private TextMesh txt;
     public int health;
     private GameObject objectPlayer;
 
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        txt = GetComponent<TextMesh>();
+        txt.text = "Vida: " + health.ToString();
         objectPlayer = GameObject.Find("Player");
     }
 
     void Update()
     {
         nav.destination = objectPlayer.transform.position;
-    }
+    } 
 
     public void takeDamage(int d){
         health -= d;
+        txt.text = "Vida: " + health.ToString();
 
         if(health <= 0){
             Destroy(this.gameObject);
