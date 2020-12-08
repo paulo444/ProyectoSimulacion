d55437f39d0f8dae447e9c6ba680f28a89e7f44e
Paulo Celis <pauloxyo@gmail.com>
2020-12-08 13:18:22 -0600

    Golden Sword & Text Rotation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,13 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent nav;
-    private TextMesh txt;
+    public TextMesh txt;
     public int health;
     private GameObject objectPlayer;
 
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-        txt = GetComponent<TextMesh>();
         txt.text = "Vida: " + health.ToString();
         objectPlayer = GameObject.Find("Player");
     }
--- Assets/Scripts/PlayerDetection.cs
+++ Assets/Scripts/PlayerDetection.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PlayerDetection : MonoBehaviour
 {
     public GameObject teleport;
+    public Damage damage;
+    public GameObject goldenSword;
 
     void OnTriggerEnter(Collider col){
         if(col.gameObject.tag == "Teleport"){
             string name = col.gameObject.GetComponent<Teleport>().getTeleportName();
             teleport.GetComponent<SceneController>().LoadLevel(name);
+        }else if(col.gameObject.tag == "Item"){
+            StartCoroutine(activateGoldenSword());
+            Destroy(col.gameObject);
         }
+    }
+
+    IEnumerator activateGoldenSword(){
+        damage.doubleDamage();
+        goldenSword.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        yield return new WaitForSeconds(15);
+        damage.setDefaultDamage();
+        goldenSword.GetComponent<MeshRenderer>().material.color = Color.white;
     }
 }
